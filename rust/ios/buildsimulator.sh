9d3fe7635796e9bb25fe229eb8913d63f5e9ac0b
dorianvp <dorianvp@zingolabs.org>
2025-11-19 15:36:04 -0300

    chore(`ffi`): new types for Swift

--- rust/ios/buildsimulator.sh
+++ rust/ios/buildsimulator.sh
@@ -19,8 +19,8 @@ export AR_aarch64_apple_ios_sim="${AR_SIMULATOR}"
 # export AR_x86_64_apple_ios="${AR_SIMULATOR}"
 
 cd ../lib
-cargo run --release --features="uniffi/cli" --bin uniffi-bindgen generate ./src/zingo.udl --language swift --out-dir ./Generated
 cargo build --release --target aarch64-apple-ios-sim
+cargo run --release --features="uniffi/cli" --bin uniffi-bindgen -- generate --library ../target/aarch64-apple-ios-sim/release/libzingo.dylib --language swift --out-dir ./Generated
 cargo lipo --release --targets aarch64-apple-ios-sim
 
 cp ./Generated/zingo.swift ../../ios
